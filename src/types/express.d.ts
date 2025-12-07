c55d1591e1c078a561adbbec8ad210a9d7f6f35e
Isaac Hatilima <isaachatilima@gmail.com>
2025-12-07 21:02:26 +0100

    Removed 2fa columns in user type

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,7 +3,7 @@ import {Profile, User} from "../generated/prisma/client";
 declare global {
     namespace Express {
         interface Request {
-            user: Omit<User, "password"> & { profile: Profile };
+            user: Omit<User, "password" | "two_factor_secret" | "two_factor_recovery_codes"> & { profile: Profile };
         }
     }
 }
